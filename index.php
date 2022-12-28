92c86966b3398fa471dc365cbed44a26ef0bdea9
unknown <nurmansyahbachtyar@gmail.com>
2022-12-28 18:34:38 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,276 @@
+<?php require_once "layout/header.php"; ?>
+
+<body>
+    <!-- Navbar Start -->
+    <?php require_once "layout/navbar.php"; ?>
+    <!-- Navbar End -->
+
+    <!-- Header Start -->
+    <div class="container-fluid px-0 px-md-5 mb-5"
+        style="background-image: url(assets/img/1.jpg); width: 1280px; height:700px;">
+        <div class="row align-items-center px-3">
+            <div class="col-lg-6 text-center text-lg-left">
+                <h4 class="text-white mb-4 mt-5 mt-lg-0">Kids Learning Center</h4>
+                <h1 class="display-3 font-weight-bold text-white">
+                    New Approach to Kids Education
+                </h1>
+                <p class="text-white mb-4">
+                    Sea ipsum kasd eirmod kasd magna, est sea et diam ipsum est amet sed
+                    sit. Ipsum dolor no justo dolor et, lorem ut dolor erat dolore sed
+                    ipsum at ipsum nonumy amet. Clita lorem dolore sed stet et est justo
+                    dolore.
+                </p>
+                <a href="" class="btn btn-secondary mt-1 py-3 px-5">Learn More</a>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+
+    <!-- Facilities Start -->
+    <div class="container-fluid pt-2">
+        <div class="container pb-3">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 pb-1">
+                    <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px">
+                        <i class="flaticon-050-fence h1 font-weight-normal text-primary mb-3"></i>
+                        <div class="pl-4">
+                            <h4>Layanan Masyarakat</h4>
+                            <p class="m-0">
+                                Kasd labore kasd et dolor est rebum dolor ut, clita dolor vero
+                                lorem amet elitr vero...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-1">
+                    <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px">
+                        <i class="flaticon-022-drum h1 font-weight-normal text-primary mb-3"></i>
+                        <div class="pl-4">
+                            <h4>APBDesa</h4>
+                            <p class="m-0">
+                                Kasd labore kasd et dolor est rebum dolor ut, clita dolor vero
+                                lorem amet elitr vero...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-1">
+                    <div class="d-flex bg-light shadow-sm border-top rounded mb-4" style="padding: 30px">
+                        <i class="flaticon-030-crayons h1 font-weight-normal text-primary mb-3"></i>
+                        <div class="pl-4">
+                            <h4>Pembangunan Desa</h4>
+                            <p class="m-0">
+                                Kasd labore kasd et dolor est rebum dolor ut, clita dolor vero
+                                lorem amet elitr vero...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Facilities Start -->
+
+    <!-- Registration Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7 mb-5 mb-lg-0">
+                    <p class="section-title pr-5">
+                        <span class="pr-2">Profil</span>
+                    </p>
+                    <h1 class="mb-4">Desa Bolo - Kecamatan Kare <br>Kabupaten Madiun</h1>
+                    <p>
+                        Website desa Bolo dibangun dengan tujuan sebagai media pelayanan publik resmi desa, yang
+                        dibangun dan dikelola oleh tim desa setempat. Dengan memanfaatkan website penyelenggaraan
+                        pelayanan publik dapat dilakukan secara cepat dan mudah.
+                    </p>
+                    <a href="" class="btn btn-primary mt-4 py-2 px-4">Selengkapnya</a>
+                </div>
+                <div class="col-lg-5">
+                    <div class="card border-0">
+                        <div class="card-header bg-secondary text-center p-4">
+                            <h2 class="text-white m-0">Kirim Pesan Kepada Kami</h2>
+                        </div>
+                        <div class="card-body rounded-bottom bg-primary p-5">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" class="form-control border-0 p-4" placeholder="Nama Anda"
+                                        required="required" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control border-0 p-4" placeholder="Email Anda"
+                                        required="required" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control border-0 p-4" placeholder="Tulis Disini"
+                                        required="required" />
+                                </div>
+                                <div>
+                                    <button class="btn btn-secondary btn-block border-0 py-3" type="submit">
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Registration End -->
+    <!-- Blog Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="text-center pb-2">
+                <p class="section-title px-5">
+                    <span class="px-2">Berita Terkini</span>
+                </p>
+                <h1 class="mb-4">Berita Terkini Dari Blog</h1>
+            </div>
+            <div class="row pb-3">
+                <div class="col-lg-4 mb-4">
+                    <div class="card border-0 shadow-sm mb-2">
+                        <img class="card-img-top mb-2" src="assets/img/blog-1.jpg" alt="" />
+                        <div class="card-body bg-light text-center p-4">
+                            <h4 class="">Diam amet eos at no eos</h4>
+                            <div class="d-flex justify-content-center mb-3">
+                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Admin</small>
+                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Web Design</small>
+                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> 15</small>
+                            </div>
+                            <p>
+                                Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                                eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                                lorem. Tempor ipsum justo amet stet...
+                            </p>
+                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card border-0 shadow-sm mb-2">
+                        <img class="card-img-top mb-2" src="assets/img/blog-2.jpg" alt="" />
+                        <div class="card-body bg-light text-center p-4">
+                            <h4 class="">Diam amet eos at no eos</h4>
+                            <div class="d-flex justify-content-center mb-3">
+                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Admin</small>
+                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Web Design</small>
+                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> 15</small>
+                            </div>
+                            <p>
+                                Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                                eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                                lorem. Tempor ipsum justo amet stet...
+                            </p>
+                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card border-0 shadow-sm mb-2">
+                        <img class="card-img-top mb-2" src="assets/img/blog-3.jpg" alt="" />
+                        <div class="card-body bg-light text-center p-4">
+                            <h4 class="">Diam amet eos at no eos</h4>
+                            <div class="d-flex justify-content-center mb-3">
+                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Admin</small>
+                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Web Design</small>
+                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> 15</small>
+                            </div>
+                            <p>
+                                Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam
+                                eirmod, duo et sed sit eirmod kasd clita tempor dolor stet
+                                lorem. Tempor ipsum justo amet stet...
+                            </p>
+                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Blog End -->
+    <!-- Team Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="text-center pb-2">
+                <p class="section-title px-5">
+                    <span class="px-2">Struktur Organisasi</span>
+                </p>
+                <h1 class="mb-4">Perangkat Desa</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style="border-radius: 100%">
+                        <img class="img-fluid w-100" src="assets/img/team-1.jpg" alt="" />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light text-center px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <h4>Julia Smith</h4>
+                    <i>Music Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style="border-radius: 100%">
+                        <img class="img-fluid w-100" src="assets/img/team-2.jpg" alt="" />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light text-center px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <h4>Jhon Doe</h4>
+                    <i>Language Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style="border-radius: 100%">
+                        <img class="img-fluid w-100" src="assets/img/team-3.jpg" alt="" />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light text-center px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <h4>Mollie Ross</h4>
+                    <i>Dance Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style="border-radius: 100%">
+                        <img class="img-fluid w-100" src="assets/img/team-4.jpg" alt="" />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light text-center px-0" style="width: 38px; height: 38px"
+                                href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <h4>Donald John</h4>
+                    <i>Art Teacher</i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Team End -->
+
+
+
+    <!-- Footer Start -->
+    <?php require_once "layout/footer.php"; ?>
+    <!-- Footer End -->
---
+++ layout/navbar.php
@@ -0,0 +1,49 @@
+<div class="container-fluid bg-light position-relative shadow">
+    <nav class="navbar fixed-top navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5">
+        <a href="" class="navbar-brand font-weight-bold text-secondary">
+            <img class="" style="width: 50px;" src="assets/img/logo.png" alt="">
+            <a href="" style="text-decoration: none;">
+                <strong>Desa Bolo</strong>
+                <br>Kabupaten Madiun
+            </a>
+        </a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav font-weight-bold mx-auto py-0" style="padding-left:200px;">
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" style="padding: 30px 10px;"
+                        data-toggle="dropdown">Profil Desa</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="blog.html" class="dropdown-item">Tentang Kami</a>
+                        <a href="single.html" class="dropdown-item">Visi & Misi</a>
+                        <a href="blog.html" class="dropdown-item">Sejarah Desa</a>
+                        <a href="blog.html" class="dropdown-item">Geografis Desa</a>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" style="padding: 30px 10px;"
+                        data-toggle="dropdown">Pemerintahan</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="blog.html" class="dropdown-item">Struktur Organisasi</a>
+                        <a href="single.html" class="dropdown-item">Perangkat Desa</a>
+                        <a href="blog.html" class="dropdown-item">Lembaga Desa</a>
+                    </div>
+                </div>
+                <a href="class.html" class="nav-item nav-link" style="padding: 30px 10px;">Layanan</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" style="padding: 30px 10px;"
+                        data-toggle="dropdown">Informasi</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="blog.html" class="dropdown-item">Blog Grid</a>
+                        <a href="single.html" class="dropdown-item">Blog Detail</a>
+                    </div>
+                </div>
+                <a href="class.html" class="nav-item nav-link" style="padding: 30px 10px;">Potensi Desa</a>
+                <a href="gallery.html" class="nav-item nav-link" style="padding: 30px 10px;">Produk Hukum</a>
+            </div>
+            <!-- <a href="" class="btn btn-primary px-4">Lokasi</a> -->
+        </div>
+    </nav>
+</div>
